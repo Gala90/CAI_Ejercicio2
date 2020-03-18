73e043a1e60cdb5a9b54cd2a7011067c84e5be9f
Gala <galamagdalena@hotmail.com>
2020-03-17 23:10:19 -0300

    Ej 2

---
+++ Ejercicio2/Ejercicio2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int j;
+            int k = 0;
+
+            for (i = 1; i < 103; i++)
+            {
+              for (j = 1; j <= i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        k++;
+                    }
+                }
+
+                if (k == 2 && i % 3 == 0)
+                        {
+                         Console.WriteLine(i + " es primo y divisible por 3");
+                        }
+                else { 
+                    if (k == 2)
+                    {
+                        Console.WriteLine(i + " es primo");
+                    }
+
+                    if (i % 3 == 0)
+                    {
+                        Console.WriteLine(i + " divisible por 3");
+                    }
+                }
+
+                k = 0;
+
+
+                        }
+            Console.ReadKey();
+
+        }
+    }
+}
